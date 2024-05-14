69408cbd2ff1569ebcbe83178509324be3a6d612
Rafael <eng.rsgomes@gmail.com>
2024-05-14 15:27:45 -0300

    fix build

--- react/buddylink-explorer/src/ui/Tables/index.tsx
+++ react/buddylink-explorer/src/ui/Tables/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import Search from "../../components/Search";
 import { useBuddyState, getOrganizationAccounts } from "buddy.link";
 import useSearch from "./useSearch";
