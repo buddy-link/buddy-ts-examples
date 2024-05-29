94346710952d51738abfd693efccf1759f87412a
Rafael <eng.rsgomes@gmail.com>
2024-05-29 02:35:54 -0300

    add shadcn form

---
+++ next/sponsaru/src/components/create-quest-dialog.tsx
@@ -0,0 +1,99 @@
+import { Button } from './ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './ui/dialog';
+import CircularProgressBar from './circular-progress-bar';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui/tooltip';
+
+type CreateQuest = {
+  name: string;
+  config: {
+    pointValue: number;
+    repeatFrequency: 'none';
+    useLastSuccessAsEpoch: boolean;
+    MaximumTimeToComplete: number;
+    timezone: string;
+    repeatLimit: number;
+  };
+  image: string;
+  displayName: string;
+  owner: string;
+  trigger: {
+    type: string;
+    provider: string;
+    args: Record<string, unknown>;
+  };
+};
+
+const QuestsDialog = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button type="button" variant="primary" className="text-white gap-2 px-6 py-4">
+          Create
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent className="max-w-screen-lg">
+        <DialogHeader>
+          <DialogTitle className="font-bold text-5xl">Quests</DialogTitle>
+        </DialogHeader>
+        <DialogDescription className="font-bold">
+          &gt; Complete quests to increase personal & team points
+        </DialogDescription>
+
+        <div className="grid grid-cols-3 gap-5">
+          {quests.map((quest, index) => (
+            <div
+              key={quest.name}
+              className="shadow-[0px_-4px_0px_0px_#ff9b61_inset] rounded-md border-x-2 border-t-2 border-[#FCF4EE] flex justify-between gap-6 p-[0.625rem]"
+            >
+              <div className="flex gap-2 items-center justify-center">
+                <CircularProgressBar percentage={70} text={100} />
+                <div className="flex flex-col items-start justify-center whitespace-nowrap">
+                  <span className="text-xs">
+                    {quest.displayName.length >= 16 ? (
+                      <TooltipProvider delayDuration={300}>
+                        <Tooltip>
+                          <TooltipTrigger asChild>
+                            <span className="cursor-pointer">{quest.displayName.slice(0, 12)}...</span>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p> {quest.displayName}</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    ) : (
+                      quest.displayName
+                    )}
+                  </span>
+                  <p className="text-[0.625rem]">
+                    Time left: <span className="text-light-primary">{'17m 23s'}</span>
+                  </p>
+                </div>
+              </div>
+              <Button variant="primary" className="text-white gap-2 px-6 py-4">
+                Claim
+              </Button>
+            </div>
+          ))}
+        </div>
+
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="primary">Close</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default QuestsDialog;
